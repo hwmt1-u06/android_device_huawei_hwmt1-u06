53795d390c1c535c007a1a78a76a6b911cb62e61
BangL <henno.rickowski@gmail.com>
2015-10-16 05:57:31 +0200

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,22 @@
+# bootanimation target
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Release name
+PRODUCT_RELEASE_NAME := MT1-U06
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/hwmt1_u06/device_hwmt1_u06.mk)
+
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hwmt1_u06
+PRODUCT_NAME := cm_hwmt1_u06
+PRODUCT_BRAND := huawei
+PRODUCT_MODEL := MT1-U06
+PRODUCT_MANUFACTURER := HUAWEI
+
+
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo cm_hwmt1_u06-userdebug
+add_lunch_combo full_hwmt1_u06-userdebug
